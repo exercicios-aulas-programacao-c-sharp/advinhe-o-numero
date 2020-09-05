d9e667b45851b30a7c8581e4a9f7b5d95757181a
diego_neri <diego@diegoneri.com>
2020-09-04 22:03:01 -0300

    Finaliza o programa Advinha o Número

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace advinhe_o_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Você consegue advinhar o número que estou pensando? Dica: é um número entre 0 e 10");
+            Console.ReadKey();
+            Console.WriteLine("Será que você descobriu? Eu duvido!");
+            Console.ReadKey();
+            Console.WriteLine("O número é...");
+            Console.ReadKey();
+            Console.WriteLine("8");
+        }
+    }
+}
